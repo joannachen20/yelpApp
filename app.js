aa97ddc407ee271d82601cccb012f38f70ba412f
Joanna Chen <34517187+joannachen20@users.noreply.github.com>
2018-11-12 22:31:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let express = require('express');
 let app = express();
+let fs = require('fs');
+let data = require('./data.json');
 let ejs = require('ejs');
-var config = require('./config.js');
+let config = require('./config.js');
 const bodyParser = require('body-parser');
 let request = require('request');
 //let yelpAPI = require('yelp-api');
 let apiKey = '[API_KEY]';
 const yelp = require('yelp-fusion');
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'))
 
 app.listen(3000, () => {
     console.log("loading. . .");
@@ -16,24 +19,39 @@ app.listen(3000, () => {
 
 app.set('view engine', 'ejs');
 
-app.get('', (req, res) => {
-  res.render('index', {results: null, error: null});
+app.get('/login', (req, res) => {
+  res.render('login');
+})
+
+app.get('/index', (req, res) => {
+res.render('index', {results: null, error: null});
 })
 
 'use strict';
 
-app.post('/', (req, res) => {
+app.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let user = {
+    email: `${email}`,
+    password: `${password}`
+  };
+  let info = JSON.stringify(user);
+  fs.appendFile('data.json', info);
+  console.log("User saved.")
+  res.redirect('/index');
+});
+
+app.post('/index', (req, res) => {
 let term = req.body.term;
 let location = req.body.location;
 let searchRequest = {
-  // term: `${term}`,
-  // location: `${location}`
   term:` ${term}`,
   location: `${location}`
-};
+}
 const client = yelp.client(apiKey);
 client.search(searchRequest).then(response => {
-  console.log(response);
+  //console.log(response);
   const result = response.jsonBody.businesses[0].name;
   let output = `Your first match is ${result}`;
   res.render('index', {results: output, error: null});
@@ -41,76 +59,3 @@ client.search(searchRequest).then(response => {
   console.log(err);
 });
 });
-
-// app.post('/', (req, res) => {
-//   let term = req.body.term;
-//   let location = req.body.location;
-//   //let url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`;
-//   let options = {
-//     method: 'GET',
-//     url: `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`,
-//     headers: {
-//       Authorization: 'Bearer ' + apiKey }
-//      };
-// request(options, (err, response, body) => {
-//   if(err) {
-//     res.render('index', {results: null, error: 'Error'})
-//   } else {
-//     let results = JSON.parse(body);
-//     console.log(results);
-//     //let firstMatch = `Your first match is ${results.businesses[0].name}`;
-//     //res.render('index', {results: firstMatch, error: null});
-//   }
-//   })
-// });
-
-// app.post('/', (req, res) => {
-//   let term = req.body.term;
-//   let location = req.body.location;
-//   let url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`;
-//   request(url, (err, response, body) => {
-//     if(err) {
-//       res.render('index', {results: null, error: 'Error'})
-//     } else {
-//       let data = JSON.parse(body);
-//       console.log(data);
-//     }
-//   })
-// });
-
-// app.post('/', (req, res, next) => {
-//   let term = req.body.term;
-//   let location = req.body.location;
-//   let url = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}`;
-//   request(url, (err, response, body) => {
-//     if (err) {
-//       res.render('index', {results: null, error: 'error'});
-//     } else {
-//       let data = JSON.parse(body);
-//       //let firstMatch = `Your first match is ${response.jsonBody.businesses[0].name}`;
-//       //res.render('index', {results: firstMatch, error: null});
-//       console.log(data);
-//     }
-//   });
-// });
-// app.post('/', (req, res) => {
-//   let params = [{ location: '${loc}' }];
-//   let loc = req.body.loc;
-//   let url = 'https://api.yelp.com/v3/businesses/search';
-//   //let term = req.body.term;
-//   request(url, (err, res, body) => {
-//     yelp.query('businesses/search', params)
-//     .then(data => {
-//       // Success
-//       let results = JSON.parse(data);
-//       console.log(results);
-//       let firstMatch = `Your first match is ${results.businesses[0].name}`;
-//         res.render('index', {results: firstMatch, error: null});
-//         //console.log(results.name);
-//     })
-//     .catch(err => {
-//       // Failure
-//       console.log(err);
-//     });
-//   });
-// });
